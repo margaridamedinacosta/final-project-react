f9b4d7086ca41e648363a0b631416e66fc39d815
margaridamedinacosta <57046731+margaridamedinacosta@users.noreply.github.com>
2019-11-24 19:37:25 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was codded by{" "}
-        <a
-          href="https://www.instagram.com/margaridamedina.costa/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Maggie
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/margaridamedinacosta/final-project-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Git Hub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was codded by{" "}
+          <a
+            href="https://www.instagram.com/margaridamedina.costa/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maggie
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/margaridamedinacosta/final-project-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Git Hub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
