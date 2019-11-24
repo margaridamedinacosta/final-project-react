04cb40688b8857910b5d004141ddc5aa1cb206ee
margaridamedinacosta <57046731+margaridamedinacosta@users.noreply.github.com>
2019-11-24 19:25:18 +0000

    added git hub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was codded by{" "}
+        <a
+          href="https://www.instagram.com/margaridamedina.costa/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Maggie
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/margaridamedinacosta/final-project-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Git Hub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
