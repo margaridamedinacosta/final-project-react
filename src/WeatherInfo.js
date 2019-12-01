6ebc6d83111c67e686181c94213133d205b4162a
margaridamedinacosta <57046731+margaridamedinacosta@users.noreply.github.com>
2019-12-01 21:37:31 +0000

    Added Weather Icon Component
    Icon isn't displaying

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,11 +17,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div clasName="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <WeatherIcon code={props.data.icon} />
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
